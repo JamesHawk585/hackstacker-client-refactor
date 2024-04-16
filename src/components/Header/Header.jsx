56240f414f9facafdecf08687b4a560a9c0bb5b3
James Hawk <jph94880@gmail.com>
2024-04-16 12:22:06 -0500

    Adds Header.jsx and Header.css

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import BlogPage from './BlogPage'
+
+const Header = () => {
+  return (
+      <header>
+          <h1>
+              HackStacker
+              <span className="logo" role="img">
+              💻
+              </span>
+          </h1>
+      </header>
+  );
+}
+
+export default Header;
